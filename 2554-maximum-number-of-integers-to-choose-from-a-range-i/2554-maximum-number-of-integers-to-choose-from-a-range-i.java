181e19d113687f5eeba7e797b57b3c1429132841
Mogili Deepika <144080036+Deepiika8080@users.noreply.github.com>
2024-12-06 19:41:22 +0530

    Time: 89 ms (7.06%), Space: 45.5 MB (90.91%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,20 +1,33 @@
 class Solution {
+    public boolean b_Search(int key,int[] banned) {
+        int low = 0;
+        int m = banned.length;
+        int high = m-1;
+        
+        int mid;
+        while(low<=high) {
+            mid = (low + high)/2;
+            if(banned[mid] == key) {
+                return true;
+            }else if(key > banned[mid]) {
+                low = mid+1;
+            }else {
+                high = mid-1;
+            }
+        }
+        return false;
+    }
     public int maxCount(int[] banned, int n, int maxSum) {
         
-        Set<Integer> set = new HashSet<Integer>();
-        
-        // stored the elements in set
-        int m = banned.length;
-        for(int i=0;i<m;i++) {
-            set.add(banned[i]);
-        }
-        int sum = 0;
-        int count = 0;
+         Arrays.sort(banned);
+         int sum = 0;
+         int count = 0;
         for(int i=1;i<=n;i++) {
-            if(!set.contains(i) && sum + i <= maxSum ) {
-                sum += i; 
-                count++; 
-            }      
+            boolean exists = b_Search(i,banned);
+            if(!exists && sum + i <= maxSum) {
+                sum += i;
+                count++;
+            }
         }
         return count;
     }
