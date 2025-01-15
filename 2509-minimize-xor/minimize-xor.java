5ff662ed59b91f2fdf4bccc5da414b2b19ada8dc
Mogili Deepika <144080036+Deepiika8080@users.noreply.github.com>
2025-01-15 21:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (5.66%) - LeetSync

--- 2509-minimize-xor/minimize-xor.java
+++ 2509-minimize-xor/minimize-xor.java
@@ -1,38 +1,37 @@
 class Solution {
     public int SetBit(int num, int bitPos) {
-       return  num |(1 << bitPos);
+        return num | (1 << bitPos);
     }
 
     public boolean isSet(int num, int bitPos) {
-       return (num & ( 1 << bitPos)) != 0;
+        return (num & (1 << bitPos)) != 0;
+    }
+
+    public boolean isUnSet(int num, int bitPos) {
+        return (num & (1 << bitPos)) == 0;
     }
 
     public int UnSetBit(int num, int bitPos) {
-       return num & ~( 1 << bitPos);
+        return num & ~(1 << bitPos);
     }
+
     public int minimizeXor(int num1, int num2) {
-        int x = num1;
-        int currSetBits = Integer.bitCount(num1);
+        int x = 0;
+
         int requredSetBits = Integer.bitCount(num2);
-        int bit = 0;
-        if (currSetBits < requredSetBits) {
-            while (currSetBits < requredSetBits) {
-                if (!isSet(x, bit)) {
-                    x = SetBit(x, bit);
-                    currSetBits++;
-                }
-                bit++;
-            }
-        } else if (currSetBits > requredSetBits) {
-            while (currSetBits > requredSetBits) {
-                if (isSet(x, bit)) {
-                    x = UnSetBit(x, bit);
-                    currSetBits--;
-                }
-                bit++;
+        
+        for (int bit = 31; bit >= 0 && requredSetBits > 0; bit--) {
+            if (isSet(num1, bit)) {
+                x = SetBit(x, bit);
+                requredSetBits--;
             }
         }
-
+        for (int bit = 0; bit < 32 && requredSetBits > 0; bit++) {
+            if (isUnSet(num1, bit)) {
+                x = SetBit(x, bit);
+                requredSetBits--;
+            }
+        }
         return x;
     }
 }
